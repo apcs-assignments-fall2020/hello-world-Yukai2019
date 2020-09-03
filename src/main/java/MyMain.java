5948a73640c447073f028c7422f213194ef45535
Yukai2019 <ysyuki86@gmail.com>
2020-09-03 08:09:43 +0000

    Chat bot assignment

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,8 +1,23 @@
-public class MyMain {
-    public static void main(String[] args) {
-        // Write some code here!
-        System.out.println("Hello, world!");
-    
+import java.util.Scanner;
 
-    }
+public class MyMain {
+	public static void main(final String[] args) {
+
+        final Scanner scan = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        final String name = scan.next();
+        System.out.println("Hello, " + name + "!" + " It's nice to meet you!");
+        System.out.print("What is your favorite color, " + name + "?");
+        final String color = scan.next();
+        System.out.println("Oh I like " + color + " too!");
+        System.out.print("What is your favorite food, " + name + "?");
+        final String food = scan.next();
+        System.out.println("Oh " + food + " I see that's interesting!");
+        System.out.print("What is your favorite hobby, " + name + "?");
+        final String hobby = scan.next();
+        System.out.println(hobby + " It's nice to know your hobby! Thank you.");
+        System.out.print("Last question, " + name + ". If you can visit any country, Which country would you go?");
+        final String country = scan.next();
+        System.out.println("Nice " + country + ". I want to visit there as well. Thank you for answering questions and see you again." );
+}
 }
